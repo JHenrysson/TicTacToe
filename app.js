e88e99c66628e53df1b7bf21be2848dc97d298ca
JHenrysson <56089745+JHenrysson@users.noreply.github.com>
2020-08-14 09:47:44 +0200

    added js
    - array = indentify squares
    -js -  if square clicked it using index of array = gives current player

---
+++ app.js
@@ -0,0 +1,26 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const playerDisplay= document.querySelector('#player')
+
+// player assignment
+    let currentPlayer ='PlayerX'
+
+//event listener to each square in the grid
+squares.forEach(squares => {
+    squares.addEventListener('click',clickOutcome)
+})
+    // array used to identify each square
+    function clickOutcome(e){
+    const squareArray= Array.from(squares)
+        const index = squareArray.indexOf(e.target)
+        playerDisplay.innerHTML = currentPlayer
+        if(currentPlayer ==='playerX') {
+            squares[index].classList.add('playerX')
+            currentPlayer = 'playerO'
+        }else{
+            squares[index].classList.add('playerO')
+            currentPlayer = 'playerX'
+        }
+    }
+})
